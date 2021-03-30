cab7223b15210bb1072e9a563d9e63ada59e4a2b
Chris Oliver <excid3@gmail.com>
2021-03-30 08:59:29 -0500

    Reorganize polyfills

--- lib/noticed.rb
+++ lib/noticed.rb
@@ -46,22 +46,3 @@ module Noticed
     end
   end
 end
-
-# The following implements polyfills for Rails < 6.0
-module ActionCable
-  # If the Rails 6.0 ActionCable::TestHelper is missing then allow it to autoload
-  unless ActionCable.const_defined? "TestHelper"
-    autoload :TestHelper, "rails_6_polyfills/actioncable_test_helper.rb"
-  end
-  # If the Rails 6.0 test SubscriptionAdapter is missing then allow it to autoload
-  unless ActionCable.const_defined? "SubscriptionAdapter::Test"
-    module SubscriptionAdapter
-      autoload :Test, "rails_6_polyfills/actioncable_test_adapter.rb"
-    end
-  end
-end
-
-# If the Rails 6.0 ActionJob DurationSerializer is missing then allow it to autoload
-unless Object.const_defined?("ActiveJob::Serializers::DurationSerializer")
-  require File.expand_path("rails_6_polyfills/activejob_duration_serializer.rb", __dir__)
-end

361dab704604818a12a858426cd91aa51492a8d2
Chris Oliver <excid3@gmail.com>
2021-03-30 08:58:44 -0500

    Use defaults from current Rails version in tests

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,9 +7,6 @@ require "noticed"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults ENV["APPRAISAL"] == "5.2" ? 5.2 : 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

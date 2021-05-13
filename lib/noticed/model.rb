3244bc05ab9b04b52e4256886a947c954e923fe6
Adrian CB <abarbul@gmail.com>
2021-05-13 20:28:33 +1000

    Rescuing from database connection errors during the bootstrapping/compilation phase.

--- lib/noticed/model.rb
+++ lib/noticed/model.rb
@@ -1,5 +1,16 @@
 module Noticed
   module Model
+    DATABASE_ERROR_CLASS_NAMES = lambda {
+      classes = []
+      if ActiveRecord::VERSION::STRING.to_f > 4.0
+        classes << ActiveRecord::NoDatabaseError
+      end
+      classes << ActiveRecord::ConnectionNotEstablished
+      classes << Mysql2::Error     if defined?(::Mysql2)
+      classes << PG::ConnectionBad if defined?(::PG)
+      classes
+    }.call.freeze
+
     extend ActiveSupport::Concern
 
     included do
@@ -32,7 +43,9 @@ module Noticed
         else
           Noticed::TextCoder
         end
-      rescue ActiveRecord::NoDatabaseError
+      rescue *DATABASE_ERROR_CLASS_NAMES => _error
+        warn("Noticed was unable to bootstrap correctly as the database is unavailable.")
+
         Noticed::TextCoder
       end
     end

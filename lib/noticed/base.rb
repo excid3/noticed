5f0c47de1a84af0a73f6a8df037ea175fed5a4bb
fractaledmind <stephen.margheim@gmail.com>
2021-08-17 11:53:58 +0200

    Fix typo

--- lib/noticed/base.rb
+++ lib/noticed/base.rb
@@ -100,8 +100,7 @@ module Noticed
 
         # Always perfrom later if a delay is present
         if (delay = delivery_method.dig(:options, :delay))
-          delay_duration = repond_to?(delay) ? send(delay) : delay
-
+          delay_duration = respond_to?(delay) ? send(delay) : delay
           method.set(wait: delay_duration, queue: queue).perform_later(args)
         elsif enqueue
           method.set(queue: queue).perform_later(args)

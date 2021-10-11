02e61e0b195c3150aee8439fcac4c628e3b677bb
Chris Oliver <excid3@gmail.com>
2021-10-11 16:43:34 -0500

    Allow dynamic mailer to be returned if symbol passed (#161)
    * Allow dynamic mailer to be returned if symbol passed
    
    * Update changelog
    
    * Call method on notification
    
    * Dynamic mailer method option

--- lib/noticed/delivery_methods/email.rb
+++ lib/noticed/delivery_methods/email.rb
@@ -9,12 +9,33 @@ module Noticed
 
       private
 
+      # mailer: "UserMailer"
+      # mailer: UserMailer
+      # mailer: :my_method - `my_method` should return Class
       def mailer
-        options.fetch(:mailer).constantize
+        option = options.fetch(:mailer)
+        case option
+        when String
+          option.constantize
+        when Symbol
+          notification.send(option)
+        else
+          option
+        end
       end
 
+      # Method should be a symbol
+      #
+      # If notification responds to symbol, call that method and use return value
+      # If notification does not respond to symbol, use the symbol for the mailer method
+      # Otherwise, use the underscored notification class name as the mailer method
       def method
-        options[:method] || notification.class.name.underscore
+        method_name = options[:method]&.to_sym
+        if method_name.present?
+          notification.respond_to?(method_name) ? notification.send(method_name) : method_name
+        else
+          notification.class.name.underscore
+        end
       end
 
       def format

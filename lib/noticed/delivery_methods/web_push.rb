c265d90cb433dd27796b4b0a67a4a4a1bb53d36d
Jonathan Bennett <jonathan@jbennett.me>
2023-07-08 00:26:09 -0400

    add option for where notification data is coming from

--- lib/noticed/delivery_methods/web_push.rb
+++ lib/noticed/delivery_methods/web_push.rb
@@ -1,9 +1,11 @@
 module Noticed
   module DeliveryMethods
     class WebPush < Noticed::DeliveryMethods::Base
+      option :data_method
+
       def deliver
         recipient.web_push_subscriptions.each do |subscription|
-          subscription.publish(notification.data)
+          subscription.publish(data)
         rescue ::WebPush::ExpiredSubscription
           Rails.logger.info "Removing expired WebPush subscription"
           subscription.destroy
@@ -11,6 +13,12 @@ module Noticed
           Rails.logger.info "Removing unauthorized WebPush subscription"
           subscription.destroy
         end
+      end
+
+      private
+
+      def data
+        notification.send(options.fetch(:data_method, :web_push_data))
       end
     end
   end

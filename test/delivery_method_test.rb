170d9e0d4084fd579644b05681bd758771d3a563
Chris Oliver <excid3@gmail.com>
2024-01-18 11:08:41 -0600

    Fix test

--- test/delivery_method_test.rb
+++ test/delivery_method_test.rb
@@ -10,8 +10,8 @@ class DeliveryMethodTest < ActiveSupport::TestCase
     assert_equal UserMailer, @delivery_method.fetch_constant(:mailer)
   end
 
-  test "delivery methods inhiert required options" do
-    assert_equal [:channel, :stream, :message], InheritedDeliveryMethod.required_option_names
+  test "delivery methods inherit required options" do
+    assert_equal [:message], InheritedDeliveryMethod.required_option_names
   end
 
   test "if config" do

eb003206db0d3f73aaea18e8efade0645a19e07f
Chris Oliver <excid3@gmail.com>
2024-01-19 20:47:24 -0600

    Check class collisions for Notifier generator
    Like Rails, these commands should be equivalent:
    
        rails g noticed:notifier Example
        rails g noticed:notifier ExampleNotifier
    
    They should both produce `app/notifiers/example_notifier.rb` and contain
    the `class ExampleNotifier` definition.
    
    Rails does this using `check_class_collision suffix: "Notifier"` and
    adjusting the filename and templates to ensure the notifier is included
    there.

--- lib/generators/noticed/notifier_generator.rb
+++ lib/generators/noticed/notifier_generator.rb
@@ -7,12 +7,20 @@ module Noticed
     class NotifierGenerator < Rails::Generators::NamedBase
       include Rails::Generators::ResourceHelpers
 
+      check_class_collision suffix: "Notifier"
+
       source_root File.expand_path("../templates", __FILE__)
 
       desc "Generates a notification with the given NAME."
 
       def generate_notification
-        template "notifier.rb", "app/notifiers/#{file_path}.rb"
+        template "notifier.rb", "app/notifiers/#{file_path}_notifier.rb"
+      end
+
+      private
+
+      def file_name # :doc:
+        @_file_name ||= super.sub(/_notifier\z/i, "")
       end
     end
   end

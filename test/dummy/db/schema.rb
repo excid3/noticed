21e8f91412716f9753c462f156dd6c993a2f9ae3
Chris Oliver <excid3@gmail.com>
2024-01-24 09:50:58 -0600

    Add tests for STI recipients to ensure they save the base_class to Noticed::Notification

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2023_12_15_202924) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "type"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

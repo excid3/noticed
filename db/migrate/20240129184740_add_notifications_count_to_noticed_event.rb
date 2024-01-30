2340c236202f4b36fae8ba449fcfc82de13a53d9
anton-shch <anton.shchankin@yandex.ru>
2024-01-30 14:00:25 +0300

    notifications_count already exists issue fix

--- db/migrate/20240129184740_add_notifications_count_to_noticed_event.rb
+++ db/migrate/20240129184740_add_notifications_count_to_noticed_event.rb
@@ -1,5 +1,5 @@
 class AddNotificationsCountToNoticedEvent < ActiveRecord::Migration[7.1]
   def change
-    add_column :noticed_events, :notifications_count, :integer
+    add_column :noticed_events, :notifications_count, :integer, if_not_exists: true
   end
 end

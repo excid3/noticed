abd9603916268f5d46fc8981dd3d46f413592e3d
Ryan Romanchuk <rromanchuk@gmail.com>
2024-02-14 11:00:12 -0600

    Lint fixes

--- db/migrate/20231215190233_create_noticed_tables.rb
+++ db/migrate/20231215190233_create_noticed_tables.rb
@@ -25,11 +25,12 @@ class CreateNoticedTables < ActiveRecord::Migration[6.1]
   end
 
   private
-    def primary_and_foreign_key_types
-      config = Rails.configuration.generators
-      setting = config.options[config.orm][:primary_key_type]
-      primary_key_type = setting || :primary_key
-      foreign_key_type = setting || :bigint
-      [primary_key_type, foreign_key_type]
-    end
+
+  def primary_and_foreign_key_types
+    config = Rails.configuration.generators
+    setting = config.options[config.orm][:primary_key_type]
+    primary_key_type = setting || :primary_key
+    foreign_key_type = setting || :bigint
+    [primary_key_type, foreign_key_type]
+  end
 end

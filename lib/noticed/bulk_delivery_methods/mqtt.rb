43f10567e79dfbaaa3a7df0b1e5dab0b61130e81
Jay Ang <zijie@nimblehq.co>
2025-05-30 16:33:38 +0700

    * Add AWS IoT support

--- lib/noticed/bulk_delivery_methods/mqtt.rb
+++ lib/noticed/bulk_delivery_methods/mqtt.rb
@@ -1,55 +1,78 @@
+# frozen_string_literal: true
+
+require 'aws-sdk-iotdataplane'
+require 'mqtt'
+
 module Noticed
   module BulkDeliveryMethods
     class Mqtt < BulkDeliveryMethod
-      required_options :url, :message
+      DEFAULT_MQTT_PORTS = { mqtt: 1883, mqtts: 8883 }.freeze
+      DEFAULT_QOS_RANGE = (0..2)
+      AWS_IOT_QOS_RANGE = (0..1)
+
+      required_options :provider, :url, :message
 
       def deliver
-        host, port, tls = parse_url(evaluate_option(:url))
-        message = evaluate_option(:message)
+        provider = evaluate_option(:provider).to_s
+        endpoint = evaluate_option(:url)
+        message  = evaluate_option(:message)
+
+        case provider
+        when 'aws_iot'
+          publish_via_aws_iot(endpoint, message)
+        else
+          publish_via_mqtt_broker(endpoint, message)
+        end
+      end
+
+      private
+
+      def publish_via_aws_iot(endpoint, message)
+        credentials = evaluate_option(:credentials)
+
+        client = Aws::IoTDataPlane::Client.new(
+          endpoint:,
+          region: credentials[:region],
+          credentials: Aws::Credentials.new(credentials[:access_key_id], credentials[:secret_access_key])
+        )
+
+        client.publish(
+          topic: message.fetch(:topic),
+          payload: serialise_payload(message.fetch(:payload)),
+          qos: messamessage.fetch(:qos, 0).to_i.clamp(*AWS_IOT_QOS_RANGE),
+          retain: message.fetch(:retain, false)
+        )
+      end
+
+      def publish_via_mqtt_broker(endpoint, message)
+        host, port, tls = parse_url(endpoint)
+        credentials = evaluate_option(:credentials)
 
         params = {
-          host: host,
-          port: port,
-          client_id: client_id,
-          ssl: tls
-        }.merge(auth_opts)
+          host:,
+          port:,
+          client_id:,
+          ssl: tls,
+          username: credentials[:username],
+          password: credentials[:password]
+        }
 
         MQTT::Client.connect(**params) do |client|
           client.publish(
             message.fetch(:topic),
             serialise_payload(message.fetch(:payload)),
             message.fetch(:retain, false),
-            message.fetch(:qos, 0).to_i.clamp(0, 2)
+            message.fetch(:qos, 0).to_i.clamp(*DEFAULT_QOS_RANGE)
           )
         end
       end
 
-      private
-
-      def auth_opts
-        case evaluate_option(:provider).to_sym
-        when :emqx
-          basic_auth = evaluate_option(:basic_auth)
-          {
-            username: basic_auth[:user],
-            password: basic_auth[:pass]
-          }
-        else
-          basic_auth = evaluate_option(:basic_auth)
-          {
-            username: basic_auth[:user],
-            password: basic_auth[:pass]
-          }
-        end
-      end
-
       def parse_url(raw)
-        raw = "mqtt://#{raw}" unless raw[%r{^\w+://}]
-        uri = URI(raw)
+        uri = URI(raw.include?('://') ? raw : "mqtt://#{raw}")
 
         host = uri.host
-        port = uri.port || (uri.scheme == "mqtts" ? 8883 : 1883)
-        tls = (uri.scheme == "mqtts") || port == 8883
+        port = uri.port || DEFAULT_MQTT_PORTS[uri.scheme&.to_sym] || 1883
+        tls = uri.scheme == 'mqtts'
 
         [host, port, tls]
       end
--- noticed.gemspec
+++ noticed.gemspec
@@ -17,5 +17,6 @@ Gem::Specification.new do |spec|
   spec.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
   spec.add_dependency "rails", ">= 6.1.0"
+  spec.add_dependency "aws-sdk-iotdataplane", "~> 1.75"
   spec.add_dependency "mqtt", "~> 0.6.0"
 end

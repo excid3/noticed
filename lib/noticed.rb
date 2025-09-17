c5a31db6c93fd8bf20584f1169d9d2f227afd07b
Chris Oliver <excid3@gmail.com>
2025-09-17 16:16:35 -0500

    Use zeitwerk to load gem

--- lib/noticed.rb
+++ lib/noticed.rb
@@ -1,6 +1,12 @@
 require "noticed/version"
 require "noticed/engine"
 
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
+loader.ignore("#{__dir__}/generators")
+loader.setup
+
 module Noticed
   include ActiveSupport::Deprecation::DeprecatedConstantAccessor
 
@@ -10,44 +16,15 @@ module Noticed
 
   deprecate_constant :Base, "Noticed::Event", deprecator: deprecator
 
-  autoload :ApiClient, "noticed/api_client"
-  autoload :BulkDeliveryMethod, "noticed/bulk_delivery_method"
-  autoload :Coder, "noticed/coder"
-  autoload :DeliveryMethod, "noticed/delivery_method"
-  autoload :HasNotifications, "noticed/has_notifications"
-  autoload :NotificationChannel, "noticed/notification_channel"
-  autoload :RequiredOptions, "noticed/required_options"
-  autoload :Translation, "noticed/translation"
-
-  module BulkDeliveryMethods
-    autoload :Bluesky, "noticed/bulk_delivery_methods/bluesky"
-    autoload :Discord, "noticed/bulk_delivery_methods/discord"
-    autoload :Slack, "noticed/bulk_delivery_methods/slack"
-    autoload :Test, "noticed/bulk_delivery_methods/test"
-    autoload :Webhook, "noticed/bulk_delivery_methods/webhook"
-  end
-
   module DeliveryMethods
     include ActiveSupport::Deprecation::DeprecatedConstantAccessor
     deprecate_constant :Base, "Noticed::DeliveryMethod", deprecator: Noticed.deprecator
-
-    autoload :ActionCable, "noticed/delivery_methods/action_cable"
-    autoload :Email, "noticed/delivery_methods/email"
-    autoload :Fcm, "noticed/delivery_methods/fcm"
-    autoload :Ios, "noticed/delivery_methods/ios"
-    autoload :MicrosoftTeams, "noticed/delivery_methods/microsoft_teams"
-    autoload :Slack, "noticed/delivery_methods/slack"
-    autoload :Test, "noticed/delivery_methods/test"
-    autoload :TwilioMessaging, "noticed/delivery_methods/twilio_messaging"
-    autoload :VonageSms, "noticed/delivery_methods/vonage_sms"
-    autoload :Webhook, "noticed/delivery_methods/webhook"
   end
 
   mattr_accessor :parent_class
   @@parent_class = "Noticed::ApplicationJob"
 
-  class ValidationError < StandardError
-  end
+  class ValidationError < StandardError; end
 
   class ResponseUnsuccessful < StandardError
     attr_reader :response

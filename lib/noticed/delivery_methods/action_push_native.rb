b3f1777a2b4eb2c535ed59749b5abe14261dae9d
Chris Oliver <excid3@gmail.com>
2025-10-03 15:23:30 -0500

    Add Action Push Native delivery method (#555)
    * Add Action Push Native delivery method
    
    * Only set app deprecator for supported rails versions
    
    * Add silent and fix notification instantiation
    
    * Refactor config to match factory methods
    
    * Update notes
    
    * Fix deliver later for APN
    
    * Remove app deprecator
    
    * Revert deprecator version

---
+++ lib/noticed/delivery_methods/action_push_native.rb
@@ -0,0 +1,24 @@
+module Noticed
+  module DeliveryMethods
+    class ActionPushNative < DeliveryMethod
+      required_options :devices, :format
+
+      def deliver
+        notification = evaluate_option(:silent) ? notification_class : notification_class.silent
+
+        notification
+          .with_apple(evaluate_option(:with_apple))
+          .with_google(evaluate_option(:with_google))
+          .with_data(evaluate_option(:with_data))
+          .new(**evaluate_option(:format))
+          .deliver_later_to(evaluate_option(:devices))
+      end
+
+      def notification_class
+        fetch_constant(:class) || ApplicationPushNotification
+      end
+    end
+  end
+end
+
+ActiveSupport.run_load_hooks :noticed_delivery_methods_action_push_native, Noticed::DeliveryMethods::ActionPushNative

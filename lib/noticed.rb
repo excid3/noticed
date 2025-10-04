1a0c544772f4c6f05c3380a0df87f4f69c922ae1
Chris Oliver <excid3@gmail.com>
2025-10-04 07:12:52 -0500

    Only autoload delivery method directories. Fixes #557

--- lib/noticed.rb
+++ lib/noticed.rb
@@ -5,6 +5,8 @@ require "zeitwerk"
 
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.ignore("#{__dir__}/generators")
+loader.do_not_eager_load("#{__dir__}/noticed/bulk_delivery_methods")
+loader.do_not_eager_load("#{__dir__}/noticed/delivery_methods")
 loader.setup
 
 module Noticed
